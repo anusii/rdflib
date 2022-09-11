f66c9d2e100342dfd68b19dd81d7b730590880e3
tian <tianwu@outlook.com>
2022-09-12 09:53:18 +1000

    update methods & add comments

--- lib/src/term.dart
+++ lib/src/term.dart
@@ -27,19 +27,22 @@ class URIRef {
   }
 
   String fragment() {
+    // extract fragment after '#'
     return Uri.parse(value).fragment;
   }
 
-  void slash(String name) {
+  // add attribute, e.g., URIRef.fullUri('http://example.org').slash('donna')
+  URIRef slash(String name) {
     if (name.startsWith('/')) {
       name = name.substring(1);
     }
     // TODO: check if there's invalid char in name
     // update value
-    value += name;
+    return URIRef.fullUri(value + name);
   }
 
   bool isValidUri(String uri) {
+    // TODO: find a robust way to validate uri
     return Uri.tryParse(uri)?.hasAbsolutePath ?? false;
   }
 
@@ -49,15 +52,7 @@ class URIRef {
 
   @override
   String toString() {
-    // TODO: implement toString
-    return value;
+    // return raw form of URIRef instance
+    return '$runtimeType($value)';
   }
-}
-
-main() {
-  URIRef u1 = URIRef.fullUri('val');
-  print(u1);
-  URIRef u2 = URIRef(value: '', base: 'http://www.cecs.anu.edu.au/#bob');
-  print(u2);
-  print(u2.fragment());
 }
