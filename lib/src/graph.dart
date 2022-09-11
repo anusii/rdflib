941641825d753cd1477f04555db1f7eaad44846c
tian <tianwu@outlook.com>
2022-09-12 09:53:59 +1000

    update method

--- lib/src/graph.dart
+++ lib/src/graph.dart
@@ -7,16 +7,6 @@ class Graph {
   Set triples = {};
 
   void add(Triple triple) {
-      triples.add(triple);
+    triples.add(triple);
   }
-}
-
-main() {
-  Graph g = Graph();
-  g.add(Triple(sub: URIRef.fullUri('s'), pre: URIRef.fullUri('p'), obj: 'o'));
-  print(g.triples);
-  g.add(Triple(sub: URIRef.fullUri('s'), pre: URIRef.fullUri('p'), obj: 'o'));
-  print(g.triples);
-  g.add(Triple(sub: URIRef.fullUri('s'), pre: URIRef.fullUri('p'), obj: 'o'));
-  print(g.triples);
 }
