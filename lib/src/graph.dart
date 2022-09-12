141ce93e2efeba7f5091a40bb18cb6d19141df09
tian <tianwu@outlook.com>
2022-09-12 12:22:49 +1000

    find sub & obj based on criteria

--- lib/src/graph.dart
+++ lib/src/graph.dart
@@ -9,4 +9,24 @@ class Graph {
   void add(Triple triple) {
     triples.add(triple);
   }
+
+  Set<URIRef> subjects(URIRef pre, dynamic obj) {
+    Set<URIRef> subs = {};
+    for (Triple t in triples) {
+      if (t.pre == pre && t.obj == obj) {
+        subs.add(t.sub);
+      }
+    }
+    return subs;
+  }
+
+  Set objects(URIRef sub, URIRef pre) {
+    Set objs = {};
+    for (Triple t in triples) {
+      if (t.sub == sub && t.pre == pre) {
+        objs.add(t.obj);
+      }
+    }
+    return objs;
+  }
 }
