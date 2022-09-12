7a796c23102123802a7abc2550931239b3d7a4bf
tian <tianwu@outlook.com>
2022-09-12 17:05:49 +1000

    add namespace binding & serialzing to turtle file

--- lib/src/graph.dart
+++ lib/src/graph.dart
@@ -1,13 +1,30 @@
+import 'dart:io' show File;
+
+import 'package:rdfgraph/src/namespace.dart';
 import './term.dart';
 import './triple.dart';
 
 class Graph {
-  List<Map> graphs = [];
-  Map contexts = {};
+  Map<URIRef, Set<Triple>> graphs = {};
+  Map<String, String> contexts = {};
   Set triples = {};
 
   void add(Triple triple) {
     triples.add(triple);
+    // create a new set if key is not existed
+    if (!graphs.containsKey(triple.sub)) {
+      graphs[triple.sub] = Set();
+    }
+    graphs[triple.sub]!.add(triple);
+  }
+
+  // bind a namespace to a prefix
+  void bind(String name, Namespace ns) {
+    if (!contexts.containsKey(name)) {
+      contexts[name] = ns.ns;
+    } else {
+      throw Exception("$name already exists!");
+    }
   }
 
   Set<URIRef> subjects(URIRef pre, dynamic obj) {
@@ -28,5 +45,59 @@ class Graph {
       }
     }
     return objs;
+  }
+
+  void serialize({String format = 'ttl', String? dest}) {
+    String indent = ' ' * 4;
+    if (dest != null) {
+      var file = File(dest);
+
+      var output = StringBuffer();
+      String line = '';
+      // read and write prefixes
+      for (var c in contexts.keys) {
+        line = '@prefix $c: <${contexts[c]}> .\n';
+        output.write(line);
+      }
+
+      // read and write every graph
+      for (var k in graphs.keys) {
+        output.write('\n');
+        bool isNewGraph = true;
+        Set<Triple>? g = graphs[k];
+        for (Triple t in g!) {
+          if (isNewGraph) {
+            isNewGraph = !isNewGraph;
+            String firstHalf = '<${t.sub.value}> <${t.pre.value}>';
+            if (t.obj.runtimeType == String) {
+              line = '$firstHalf "${t.obj}" ;';
+            } else if (t.obj.runtimeType == URIRef) {
+              URIRef o = t.obj as URIRef;
+              line = '$firstHalf <${o.value}> ;';
+            } else {
+              line = '$firstHalf ${t.obj} ;';
+            }
+          } else {
+            line += '\n';
+            String firstHalf = '$indent<${t.pre.value}>';
+            if (t.obj.runtimeType == String) {
+              line += '$firstHalf "${t.obj}" ;';
+            } else if (t.obj.runtimeType == URIRef) {
+              URIRef o = t.obj as URIRef;
+              line += '$firstHalf <${o.value}> ;';
+            } else {
+              line += '$firstHalf ${t.obj} ;';
+            }
+          }
+        }
+        if (line.endsWith(';')) {
+          line = line.substring(0, line.length - 1) + '.\n';
+        }
+        output.write(line);
+      }
+      var sink = file.openWrite();
+      sink.write(output);
+      sink.close();
+    }
   }
 }
