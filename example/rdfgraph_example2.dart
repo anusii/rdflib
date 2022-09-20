a62bf3c004fea0c5638993990c75e44025f7c719
tian <tianwu@outlook.com>
2022-09-20 16:44:31 +1000

    update example to demo adding object property (linking)

--- example/rdfgraph_example2.dart
+++ example/rdfgraph_example2.dart
@@ -7,28 +7,42 @@ main() {
   Namespace shOnto =
       Namespace(ns: 'http://sii.cecs.anu.edu.au/onto/SOLID-Health#');
 
-  URIRef newSub = shData.withAttr('AssessmentTab-p43623-20220727T120913');
-  bool suc = g.addNamedIndividual(newSub);
-  print(suc);
+  URIRef newAssessTab = shData.withAttr('AssessmentTab-p43623-20220727T120913');
+  g.addNamedIndividual(newAssessTab);
 
+  // Literal string
   g.add(Triple(
-      sub: newSub, pre: RDF.type, obj: shOnto.withAttr('AssessmentTab')));
+      sub: newAssessTab, pre: RDF.type, obj: shOnto.withAttr('AssessmentTab')));
 
+  // Literal string
   g.add(Triple(
-      sub: newSub,
+      sub: newAssessTab,
       pre: shOnto.withAttr('asthmaControl'),
       obj: Literal('Poor Control')));
 
+  // Literal integer
   g.add(Triple(
-      sub: newSub,
+      sub: newAssessTab,
       pre: shOnto.withAttr('diastolicBloodPressure'),
       obj: Literal('75')));
 
+  // Literal float/double
   g.add(Triple(
-      sub: newSub,
+      sub: newAssessTab,
       pre: shOnto.withAttr('systolicBloodPressure'),
       obj: Literal('125.0')));
 
+  URIRef newSeeAndDoTab = shData.withAttr('SeeAndDoTab-p43623-20220727T120913');
+  URIRef newSeeAndDoOption =
+      shData.withAttr('SeeAndDoOption-p43623-20220727T120913-fitnessDrive');
+
+  g.addNamedIndividual(newSeeAndDoTab);
+  g.addNamedIndividual(newSeeAndDoOption);
+  // link two triple individuals
+  g.addObjectProperty(
+      newSeeAndDoTab, shOnto.withAttr('hasSeeAndDoOption'), newSeeAndDoOption);
+
+  /// binding for readability
   g.bind('sh-data', shData);
   g.bind('sh-onto', shOnto);
 
