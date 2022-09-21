000e3ecec71b061add65f55e1e6526c0bc145fcf
tian <tianwu@outlook.com>
2022-09-21 12:22:39 +1000

    add parsing local turtle file function to Graph class

--- lib/src/graph.dart
+++ lib/src/graph.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io' show File;
 
 import './namespace.dart';
@@ -129,6 +130,183 @@ class Graph {
       }
     }
     return objs;
+  }
+
+  parse(String filePath) async {
+    final file = File(filePath);
+    Stream<String> lines =
+        file.openRead().transform(utf8.decoder).transform(LineSplitter());
+    try {
+      Map<String, dynamic> config = {
+        'prefix': false,
+        'sub': URIRef('http://sub.placeholder.pl'),
+        'pre': URIRef('http://pre.placeholder.pl')
+      };
+      await for (var line in lines) {
+        /// remove leading and trailing spaces
+        line = line.trim();
+        config = _parseLine(line, config);
+      }
+    } catch (e) {
+      print('Error in parsing: $e');
+    }
+  }
+
+  Map<String, dynamic> _parseLine(String line, Map<String, dynamic> config) {
+    URIRef sub = config['sub']! as URIRef;
+    URIRef pre = config['pre']! as URIRef;
+
+    /// 1. parse prefix line to store in map contexts
+    if (line.startsWith('@') && line.endsWith('.')) {
+      /// update contexts
+      _parsePrefix(line);
+      return {'prefix': true, 'sub': sub, 'pre': pre};
+    } else {
+      /// use regex for parsing space in side quotes: \s(?=(?:[^'"`]*(['"`])[^'"`]*\1)*[^'"`]*$)
+      /// instead of just using List<String> lst = line.split(' '); which will
+      /// not work for line like 'foaf:name "Edward Scissorhands"^^xsd:string ;'
+      final re = RegExp(r'\s(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)');
+      List<String> lst = line.split(re);
+      dynamic obj;
+      if (line.endsWith(';')) {
+        /// 2. parse triple line ending with ';'
+        /// triple line with next line containing two elements of predicate and object
+        /// depending on how many elements in the line (the last one is ';')
+        if (lst.length == 3 + 1) {
+          /// full triple line with 3 elements
+          /// sub will be re-used for following lines with 2 or 1 element(s)
+          sub = _parseElement(lst[0]) as URIRef;
+
+          /// pre will be re-used for following line with 1 element
+          pre = _parseElement(lst[1]) as URIRef;
+          obj = _parseElement(lst[2]);
+
+          /// add to triples set
+          add(Triple(sub: sub, pre: pre, obj: obj));
+        } else if (lst.length == 2 + 1) {
+          /// sub is omitted with 2 elements in this line
+          pre = _parseElement(lst[0]) as URIRef;
+          obj = _parseElement(lst[1]);
+
+          /// re-use last sub
+          add(Triple(sub: sub, pre: pre, obj: obj));
+        } else {
+          throw Exception('Error: illegal line ending with ";" $line');
+        }
+      } else if (line.endsWith(',')) {
+        /// 3. parse triple line ending with ','
+        /// triple line with next line containing one element of object
+        if (lst.length == 1 + 1) {
+          obj = _parseElement(lst[0]);
+          add(Triple(sub: sub, pre: pre, obj: obj));
+        } else {
+          throw Exception('Error: illegal line ending with "," $line');
+        }
+      } else if (line.endsWith('.')) {
+        /// 4. parse triple line ending with '.'
+        if (lst.length == 3 + 1) {
+          sub = _parseElement(lst[0]) as URIRef;
+          pre = _parseElement(lst[1]) as URIRef;
+          obj = _parseElement(lst[2]);
+          add(Triple(sub: sub, pre: pre, obj: obj));
+        } else if (lst.length == 2 + 1) {
+          pre = _parseElement(lst[0]) as URIRef;
+          obj = _parseElement(lst[1]);
+          add(Triple(sub: sub, pre: pre, obj: obj));
+        } else if (lst.length == 1 + 1) {
+          obj = _parseElement(lst[0]);
+          add(Triple(sub: sub, pre: pre, obj: obj));
+        } else {
+          throw Exception('Error: illegal line ending with "." $line');
+        }
+      } else {
+        /// if it's an empty line or starts with '#', just ignore it
+        /// throw Exception('Error: cannot parse line $line');
+      }
+      return {'prefix': false, 'sub': sub, 'pre': pre};
+    }
+  }
+
+  /// first need to store prefixes to contexts map
+  void _parsePrefix(String prefixLine) {
+    if (!prefixLine.startsWith('@') || !prefixLine.endsWith('.')) {
+      throw Exception('Error: Illegal prefix $prefixLine');
+    } else if (prefixLine.toLowerCase().startsWith('@prefix') &&
+        prefixLine.endsWith('.')) {
+      /// example: ['@prefix', 'owl:', '<http://abc.com>', '.']
+      List<String> lst = prefixLine.split(' ');
+
+      /// not considering the trailing single ':' (be aware of a single ':')
+      String k = lst[1].substring(0, lst[1].length - 1);
+      String v = lst[2].substring(1, lst[2].length - 1);
+
+      /// update contexts, adding to triple will be handled by line
+      contexts[k] = v;
+    } else {
+      throw Exception('Error: unable to parse this line $prefixLine');
+    }
+  }
+
+  /// 1. parse form such as <http://www.w3.org/2002/07/owl#>
+  /// 2. parse form such as xsd:string to full URIRef
+  URIRef _toFullUriref(String s) {
+    /// case 1: <uri>
+    if (s.startsWith('<') && s.endsWith('>')) {
+      return URIRef(s.substring(1, s.length - 1));
+    } else if (s.contains(':')) {
+      /// case 2: ':'
+      if (':'.allMatches(s).length != 1) {
+        throw Exception('Error: $s does not have ":" or too many ":"');
+      } else {
+        List<String> lst = s.split(':');
+        String vocab = lst[0];
+        String type = lst[1];
+        if (!contexts.containsKey(vocab)) {
+          throw Exception('Error: $vocab not existed in contexts!');
+        } else {
+          return URIRef(contexts[vocab]! + type);
+        }
+      }
+    } else {
+      throw Exception('Error: unable to convert $s to URIRef');
+    }
+  }
+
+  /// parse single element in a triple or prefix line
+  ///
+  /// need to be more robust
+  dynamic _parseElement(String element) {
+    element = element.trim();
+
+    /// 1. <element> --> URIRef(element)
+    if (element.startsWith('<') && element.endsWith('>')) {
+      return _toFullUriref(element);
+    } else if ('"'.allMatches(element).length == 2) {
+      List<String> lst = element.split('^^');
+      String val = lst[0].substring(1, lst[0].length - 1);
+
+      /// 2. "val"^^xsd:string
+      /// need to consider case like "e.scissorhands@example.org"^^xsd:anyURI
+      if (!element.contains('@') ||
+          (element.contains('@') && element.split('@')[1].contains('.'))) {
+        if (element.contains('^^')) {
+          URIRef dType = _toFullUriref(lst[1]);
+          return Literal(val, datatype: dType);
+        } else {
+          /// 3. "val"
+          return Literal(val);
+        }
+      } else {
+        /// 4. "val"@en (exclude the above case @example.org)
+        List<String> lst = element.split('@');
+        String val = lst[0].substring(1, lst[0].length - 1);
+        String lang = lst[1];
+        return Literal(val, lang: lang);
+      }
+    } else if (element.contains(':')) {
+      /// 5. abc:def (such as rdf:type)
+      return _toFullUriref(element);
+    }
   }
 
   /// serialize the graph to certain format and export to file
