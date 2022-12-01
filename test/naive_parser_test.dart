6fd8fa56165dddd9638139124afcc5135f40b6cd
tian <tianwu@outlook.com>
2022-12-01 13:00:18 +1100

    add tests for PLX

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -251,4 +251,29 @@ main() {
       });
     });
   });
+
+  group("""[169s] 	PLX 	::= 	PERCENT | PN_LOCAL_ESC""", () {
+    Map<String, bool> testStrings = {
+      '%': false,
+      '%a9': true,
+      '%Te': false,
+      '%0': false,
+      '%D3': true,
+      '\_': false,
+      '\$': false,
+      '\\\\': false,
+      '\\"': false,
+      '\\\$': true,
+      '\\\&': true,
+      '\\@': true,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = PLX.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('PLX $element - actual: $actual, expected: $expected');
+      test('PLX case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
