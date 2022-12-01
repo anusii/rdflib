020651a9e4cab9dfe153f5db55e88f2c27f43348
tian <tianwu@outlook.com>
2022-12-01 12:37:41 +1100

    add tests for PERCENT

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -208,4 +208,24 @@ main() {
         });
       });
 
+  group(
+      """[170s] 	PERCENT 	::= 	'%' HEX HEX""",
+          () {
+        Map<String, bool> testStrings = {
+          '%': false,
+          '%a9': true,
+          '%ft': false,
+          '%8': false,
+          '%8D': true,
+        };
+        testStrings.keys.forEach((element) {
+          bool actual = PERCENT.end().accept(element);
+          bool expected = testStrings[element]!;
+          print('PERCENT $element - actual: $actual, expected: $expected');
+          test('PERCENT case $element', () {
+            expect(actual, expected);
+          });
+        });
+      });
+
 }
