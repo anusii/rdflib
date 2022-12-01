bbf4966bdf2600bef3ea480a8d8c7877e8a3df59
tian <tianwu@outlook.com>
2022-12-01 12:38:10 +1100

    dart format

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -183,49 +183,44 @@ main() {
     });
   });
 
-  group(
-      """[139s] 	PNAME_NS 	::= 	PN_PREFIX? ':'""",
-          () {
-        Map<String, bool> testStrings = {
-          ':': true,
-          'd': false,
-          'Y:': true,
-          '\u00bf': false,
-          '\u00C0': false,
-          '\u00FF:': true,
-          'b-:': true,
-          'Y507-': false,
-          'Z10.9a:': true,
-          '\u00F6\u0299.\u0300:': true,
-        };
-        testStrings.keys.forEach((element) {
-          bool actual = PNAME_NS.end().accept(element);
-          bool expected = testStrings[element]!;
-          print('PNAME_NS $element - actual: $actual, expected: $expected');
-          test('PNAME_NS case $element', () {
-            expect(actual, expected);
-          });
-        });
+  group("""[139s] 	PNAME_NS 	::= 	PN_PREFIX? ':'""", () {
+    Map<String, bool> testStrings = {
+      ':': true,
+      'd': false,
+      'Y:': true,
+      '\u00bf': false,
+      '\u00C0': false,
+      '\u00FF:': true,
+      'b-:': true,
+      'Y507-': false,
+      'Z10.9a:': true,
+      '\u00F6\u0299.\u0300:': true,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = PNAME_NS.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('PNAME_NS $element - actual: $actual, expected: $expected');
+      test('PNAME_NS case $element', () {
+        expect(actual, expected);
       });
+    });
+  });
 
-  group(
-      """[170s] 	PERCENT 	::= 	'%' HEX HEX""",
-          () {
-        Map<String, bool> testStrings = {
-          '%': false,
-          '%a9': true,
-          '%ft': false,
-          '%8': false,
-          '%8D': true,
-        };
-        testStrings.keys.forEach((element) {
-          bool actual = PERCENT.end().accept(element);
-          bool expected = testStrings[element]!;
-          print('PERCENT $element - actual: $actual, expected: $expected');
-          test('PERCENT case $element', () {
-            expect(actual, expected);
-          });
-        });
+  group("""[170s] 	PERCENT 	::= 	'%' HEX HEX""", () {
+    Map<String, bool> testStrings = {
+      '%': false,
+      '%a9': true,
+      '%ft': false,
+      '%8': false,
+      '%8D': true,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = PERCENT.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('PERCENT $element - actual: $actual, expected: $expected');
+      test('PERCENT case $element', () {
+        expect(actual, expected);
       });
-
+    });
+  });
 }
