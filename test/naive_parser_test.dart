ab8c59df3c014375ba796d6f216ab2ad4ba4ecf0
tian <tianwu@outlook.com>
2022-12-01 12:56:05 +1100

    add tests for PN_LOCAL_ESCAPE

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -223,4 +223,32 @@ main() {
       });
     });
   });
+
+  group(
+      """[172s] 	PN_LOCAL_ESC 	::= 	'\' ('_' | '~' | '.' | '-' | '!' | '\$' | '&' | "'" | '(' | ')' | '*' | '+' | ',' | ';' | '=' | '/' | '?' | '#' | '@' | '%')""",
+      () {
+    List<String> escapeChars =
+        """'_' | '~' | '.' | '-' | '!' | '\$' | '&' | "'" | '(' | ')' | '*' | '+' | ',' | ';' | '=' | '/' | '?' | '#' | '@' | '%'"""
+            .split(' | ')
+            .map((e) => e[1]) // remove surrounding char: '
+            .toList();
+    print(escapeChars);
+    final Map<String, bool> testStrings = {
+      '\_': false,
+      '\$': false,
+      '\\\\': false,
+      '\\"': false,
+      '\t': false,
+      '\ ': false,
+    };
+    escapeChars.forEach((e) => testStrings['\\$e'] = true);
+    testStrings.keys.forEach((element) {
+      bool actual = PN_LOCAL_ESC.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('PN_LOCAL_ESC $element - actual: $actual, expected: $expected');
+      test('PN_LOCAL_ESC case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
