aee2caf964e3c28e8bc284fa5e3addf9c2f06ae9
tian <tianwu@outlook.com>
2022-12-05 15:03:44 +1100

    add grammar string for turtle

--- lib/parser/naive_parser.dart
+++ lib/parser/naive_parser.dart
@@ -1,5 +1,70 @@
 import 'package:petitparser/petitparser.dart';
 
+// refer to RDF doc for complete turtle grammar: https://www.w3.org/TR/turtle/#sec-grammar-grammar
+const turtleGrammar = """
+turtle_doc: statement*
+?statement: directive | triples "."
+directive: prefix_id | base | sparql_prefix | sparql_base
+prefix_id: "@prefix" PNAME_NS IRIREF "."
+base: BASE_DIRECTIVE IRIREF "."
+sparql_base: /BASE/i IRIREF
+sparql_prefix: /PREFIX/i PNAME_NS IRIREF
+triples: subject predicate_object_list
+       | blank_node_property_list predicate_object_list?
+predicate_object_list: verb object_list (";" (verb object_list)?)*
+?object_list: object ("," object)*
+?verb: predicate | /a/
+?subject: iri | blank_node | collection
+?predicate: iri
+?object: iri | blank_node | collection | blank_node_property_list | literal
+?literal: rdf_literal | numeric_literal | boolean_literal
+blank_node_property_list: "[" predicate_object_list "]"
+collection: "(" object* ")"
+numeric_literal: INTEGER | DECIMAL | DOUBLE
+rdf_literal: string (LANGTAG | "^^" iri)?
+boolean_literal: /true|false/
+string: STRING_LITERAL_QUOTE
+      | STRING_LITERAL_SINGLE_QUOTE
+      | STRING_LITERAL_LONG_SINGLE_QUOTE
+      | STRING_LITERAL_LONG_QUOTE
+iri: IRIREF | prefixed_name
+prefixed_name: PNAME_LN | PNAME_NS
+blank_node: BLANK_NODE_LABEL | ANON
+
+BASE_DIRECTIVE: "@base"
+IRIREF: "<" (/[^\x00-\x20<>"{}|^`\\]/ | UCHAR)* ">"
+PNAME_NS: PN_PREFIX? ":"
+PNAME_LN: PNAME_NS PN_LOCAL
+BLANK_NODE_LABEL: "_:" (PN_CHARS_U | /[0-9]/) ((PN_CHARS | ".")* PN_CHARS)?
+LANGTAG: "@" /[a-zA-Z]+/ ("-" /[a-zA-Z0-9]+/)*
+INTEGER: /[+-]?[0-9]+/
+DECIMAL: /[+-]?[0-9]*/ "." /[0-9]+/
+DOUBLE: /[+-]?/ (/[0-9]+/ "." /[0-9]*/ EXPONENT
+      | "." /[0-9]+/ EXPONENT | /[0-9]+/ EXPONENT)
+EXPONENT: /[eE][+-]?[0-9]+/
+STRING_LITERAL_QUOTE: "\"" (/[^\x22\x5C\x0A\x0D]/ | ECHAR | UCHAR)* "\""
+STRING_LITERAL_SINGLE_QUOTE: "'" (/[^\x27\x5C\x0A\x0D]/ | ECHAR | UCHAR)* "'"
+STRING_LITERAL_LONG_SINGLE_QUOTE: "'''" (/'|''/? (/[^'\\]/ | ECHAR | UCHAR))* "'''"
+STRING_LITERAL_LONG_QUOTE: "\"\"\"" (/"|""/? (/[^"\\]/ | ECHAR | UCHAR))* "\"\"\""
+UCHAR: "\\u" HEX~4 | "\\U" HEX~8
+ECHAR: "\\" /[tbnrf"'\\]/
+WS: /[\x20\x09\x0D\x0A]/
+ANON: "[" WS* "]"
+PN_CHARS_BASE: /[A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\U00010000-\U000EFFFF]/
+PN_CHARS_U: PN_CHARS_BASE | "_"
+PN_CHARS: PN_CHARS_U | /[\-0-9\u00B7\u0300-\u036F\u203F-\u2040]/
+PN_PREFIX: PN_CHARS_BASE ((PN_CHARS | ".")* PN_CHARS)?
+PN_LOCAL: (PN_CHARS_U | ":" | /[0-9]/ | PLX) ((PN_CHARS | "." | ":" | PLX)* (PN_CHARS | ":" | PLX))?
+PLX: PERCENT | PN_LOCAL_ESC
+PERCENT: "%" HEX~2
+HEX: /[0-9A-Fa-f]/
+PN_LOCAL_ESC 	::= 	'\' ('_' | '~' | '.' | '-' | '!' | '\$' | '&' | "'" | '(' | ')' | '*' | '+' | ',' | ';' | '=' | '/' | '?' | '#' | '@' | '%')
+
+%ignore WS
+COMMENT: "#" /[^\n]/*
+%ignore COMMENT
+""";
+
 // [171s] 	HEX 	::= 	[0-9] | [A-F] | [a-f]
 final HEX = (pattern('a-f') | pattern('A-F') | pattern('0-9'));
 
