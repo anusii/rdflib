4d488b0ff4dff1de0e14a150399d3757864c89ef
tian <tianwu@outlook.com>
2022-12-20 13:24:29 +1100

    add tests for literal

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -721,4 +721,29 @@ main() {
       });
     });
   });
+
+  group("""[13] 	literal 	::= 	RDFLiteral | NumericLiteral | BooleanLiteral""",
+      () {
+    Map<String, bool> testStrings = {
+      '5.8': true,
+      '"Zero"': true,
+      'false': true,
+      '\'true\'@en': true,
+      '"antarctica"^^<www.wikipedia.org>': true,
+      '-1E0': true,
+      ' ': false,
+      'zero': false,
+      'true': true,
+      '"true"': true,
+      'true@en': false,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = literal.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('literal $element - actual: $actual, expected: $expected');
+      test('literal case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
