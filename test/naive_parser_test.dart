5aced543cf08e5534ee55c0d28fc2810e5076641
tian <tianwu@outlook.com>
2022-12-20 13:58:51 +1100

    add tests for BlankNode

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -853,4 +853,36 @@ main() {
       });
     });
   });
+
+  group("""[137s] 	BlankNode 	::= 	BLANK_NODE_LABEL | ANON""", () {
+    Map<String, bool> testStrings = {
+      '_:0': true,
+      '_:0.a': true,
+      '_:': false,
+      ':ar': false,
+      '_:ar': true,
+      '_:hello.dart': true,
+      '_:.ignore': false,
+      '_:_denied': true,
+      '_:_accepted.': false,
+      '_:_accepted.sub': true,
+      '[\x20]': true,
+      '[   ]': true,
+      '[\x09\x0A]': true,
+      '[]': true,
+      '[] ': false,
+      '[\x20] ': false,
+      '': false,
+      '  ': false,
+      '[  ': false,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = BlankNode.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('BlankNode $element - actual: $actual, expected: $expected');
+      test('BlankNode case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
