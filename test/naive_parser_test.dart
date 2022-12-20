15bedfbf46259a1e921e36030d6b96e93278a6d9
tian <tianwu@outlook.com>
2022-12-20 17:26:33 +1100

    add tests for directive

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -1262,4 +1262,49 @@ main() {
       });
     });
   });
+
+  group("""[3] 	directive 	::= 	prefixID | base | sparqlPrefix | sparqlBase""",
+      () {
+    Map<String, bool> testStrings = {
+      '@prefix : </etc/> .': true,
+      '@prefix c: <./> .': true,
+      '@prefix abc: <https://abc.net.au/> .': true,
+      '@prefix v2.7: <www.anu.cecs.au/> .': true,
+      '@prefix r <./> .': false,
+      '@Prefix abc: <https://abc.net.au/> .': false,
+      '@prefix v2.: <www.anu.cecs.au/> ': false,
+      'Prefix : <>': true,
+      'PREFIX : <>': true,
+      'PREFIX root: </>': true,
+      'PREFIx version5.0: <www.v5.org/>': true,
+      '@PREFIx v5: <www.v5.org/>': false,
+      'Prefix : <> .': false,
+      'PREFIX : <https://xyz.com>>': false,
+      'PREFIX root:dir </>': false,
+      'PREFIX version5.0 <www.v5.org/>': false,
+      'bAse <>': true,
+      'BasE <www.example.com>': true,
+      'Base <./> ': true,
+      'BASE <https://act.org> ': true,
+      'base <> .': false,
+      '@Base <./> ': false,
+      'BASE https://act.org ': false,
+      '@base <abc> .': true,
+      '@base <http://www.example.org> .': true,
+      '@base <> .': true,
+      '@base <./> .': true,
+      '@BASE <abc> .': false,
+      '@Base <http://www.example.org> .': false,
+      '@base <> ..': false,
+      '@base <./> ': false,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = directive.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('directive $element - actual: $actual, expected: $expected');
+      test('directive case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
