3b9eab1e4e2637222e1deac881e60745a3467ff6
tian <tianwu@outlook.com>
2022-12-20 13:56:05 +1100

    add tests for ANON

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -831,4 +831,26 @@ main() {
       });
     });
   });
+
+  group("""[162s] 	ANON 	::= 	'[' WS* ']'""", () {
+    Map<String, bool> testStrings = {
+      '[\x20]': true,
+      '[   ]': true,
+      '[\x09\x0A]': true,
+      '[]': true,
+      '[] ': false,
+      '[\x20] ': false,
+      '': false,
+      '  ': false,
+      '[  ': false,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = ANON.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('ANON $element - actual: $actual, expected: $expected');
+      test('ANON case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
