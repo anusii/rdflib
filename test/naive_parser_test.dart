fd857726d465ca454d726d10c1ee7d50017bdc5a
tian <tianwu@outlook.com>
2022-12-20 17:04:04 +1100

    add tests for prefixID

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -1200,4 +1200,24 @@ main() {
       });
     });
   });
+
+  group("""[4] 	prefixID 	::= 	'@prefix' PNAME_NS IRIREF '.'""", () {
+    Map<String, bool> testStrings = {
+      '@prefix : </etc/> .': true,
+      '@prefix c: <./> .': true,
+      '@prefix abc: <https://abc.net.au/> .': true,
+      '@prefix v2.7: <www.anu.cecs.au/> .': true,
+      '@prefix r <./> .': false,
+      '@Prefix abc: <https://abc.net.au/> .': false,
+      '@prefix v2.: <www.anu.cecs.au/> ': false,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = prefixID.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('prefixID $element - actual: $actual, expected: $expected');
+      test('prefixID case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
