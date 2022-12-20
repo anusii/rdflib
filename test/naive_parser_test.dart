462d5d7899048d0e93f4f9399fe7168f2723fb03
tian <tianwu@outlook.com>
2022-12-20 16:08:03 +1100

    add tests for blankNodePropertyList

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -1085,4 +1085,30 @@ main() {
       });
     });
   });
+
+  group("""[14] 	blankNodePropertyList 	::= 	'[' predicateObjectList ']'""", () {
+    Map<String, bool> testStrings = {
+      '[a rdf:example, <xyz.com>]': true,
+      '[ <2023> _:burg, _:_, x:, "a", <empty>, <whoiswho> ]': true,
+      '[ a <www.example.com/alice#me>, [], _:2 ]': true,
+      '[  <check#status> :_denied, _:_accepted.sub, hello:me; ;; ;]': true,
+      '[abc:time [   ], :whitespaces, "now", (:c1 :c3), 9.8; a rdf:number, owl:vocabulary ; ]':
+      true,
+      '[a rdf:example, <xyz.com> .]': false,
+      '[<2023> _:burg, _:_, x:, "a", <empty>, <whoiswho>, ]': false,
+      '[ \na <thing>; \n]': true,
+      '[a <thing>; ,<football>]': false,
+      '[a <thing>, <football>;;;]': true,
+      '[a ]': false,
+      '[]': false,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = blankNodePropertyList.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('blankNodePropertyList $element - actual: $actual, expected: $expected');
+      test('blankNodePropertyList case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
