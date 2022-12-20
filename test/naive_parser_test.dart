45ed814f194658f33ae6bf413fa5554025df3566
tian <tianwu@outlook.com>
2022-12-20 13:38:49 +1100

    add tests for verb

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -746,4 +746,42 @@ main() {
       });
     });
   });
+
+  group("""[9] 	verb 	::= 	predicate | 'a'""", () {
+    Map<String, bool> testStrings = {
+      '::': true,
+      'rdf:type': true,
+      ':xyz': true,
+      'www': false,
+      'Z10.9a:%b23c': true,
+      '_:': false,
+      '_:burg': false,
+      '_:_': false,
+      'burg:_do': true,
+      'd:': true,
+      'j:': true,
+      '': false,
+      'www:': true,
+      '<>': true,
+      '<': false,
+      '>': false,
+      '<<>': false,
+      '<{}>': false,
+      '<bob>': true,
+      '<bob#me>': true,
+      '<\u0010>': false,
+      '<www.example.com/alice#me>': true,
+      '<www.example.com/alice#me>.': false,
+      'a': true,
+      'b': false,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = verb.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('verb $element - actual: $actual, expected: $expected');
+      test('verb case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
