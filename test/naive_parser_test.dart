4ffb6906e282b41db702ceb24616138bc7684841
tian <tianwu@outlook.com>
2022-12-20 16:56:56 +1100

    add tests for base

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -1179,4 +1179,25 @@ main() {
       });
     });
   });
+
+  group("""[5] 	base 	::= 	'@base' IRIREF '.'""", () {
+    Map<String, bool> testStrings = {
+      '@base <abc> .': true,
+      '@base <http://www.example.org> .': true,
+      '@base <> .': true,
+      '@base <./> .': true,
+      '@BASE <abc> .': false,
+      '@Base <http://www.example.org> .': false,
+      '@base <> ..': false,
+      '@base <./> ': false,
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = base.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('base $element - actual: $actual, expected: $expected');
+      test('base case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
