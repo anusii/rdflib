f01e4fe0e5bb4d56466e9570f37630593f58809f
tian <tianwu@outlook.com>
2022-12-20 13:52:59 +1100

    add tests for WS

--- test/naive_parser_test.dart
+++ test/naive_parser_test.dart
@@ -809,4 +809,26 @@ main() {
       });
     });
   });
+
+  group("""[161s] 	WS 	::= 	#x20 | #x9 | #xD | #xA""", () {
+    Map<String, bool> testStrings = {
+      '\x20': true,
+      ' ': true,
+      '\x09': true,
+      '\x0D': true,
+      '\x0A': true,
+      '': false,
+      '  ': false,
+      '\x0d': true,
+      '\x09\x09': false
+    };
+    testStrings.keys.forEach((element) {
+      bool actual = WS.end().accept(element);
+      bool expected = testStrings[element]!;
+      print('WS $element - actual: $actual, expected: $expected');
+      test('WS case $element', () {
+        expect(actual, expected);
+      });
+    });
+  });
 }
